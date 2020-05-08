6102b9ecc63e4792245337fcb72299807163d164
Longjun Qin <longjunqin@gmail.com>
2020-05-08 11:22:45 +0800

    Add files via upload

---
+++ 13_Linux命令.py
@@ -0,0 +1,31 @@
+"""
+1.ctrl+shift+ +放大命令行字体  
+  ctrl+ - 缩小命令行字体
+  tab 自动补全
+2.ls 查看文件名字
+  ls -alh 查看详细信息
+3.pwd 查看当前路径
+4./ 根目录
+5.touch 1.txt 创建文件
+6.mkdir longjun 创建文件夹
+7.cd longjun 跳转目录
+  cd .. 挑上一层
+  cd - 去上一个路径
+  cd ./longjun .当前路径
+  cd ../.. 上层路径
+  cd ~ 回到家目录
+8.相对路径，绝对路径
+9.clear 清屏
+10.rm 1.txt 删除文件
+   rm -r longjun 删除文件夹  命令 选项 参数
+   rm longjun -r
+11.cp 1.txt python 拷问文件到文件夹
+   cp longjun python -r 拷贝文件夹
+12.tree 查看目录树
+13.mv 1.txt python 剪切文件
+   mv python longjun 剪切文件夹
+   mv 1.txt longjun.txt 重命名
+14.history 查看历史命名
+15.ls --help
+   man ls 查看帮助
+"""
